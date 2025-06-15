604b3b5651ce7c75a0a8ef443279d0f68c7e2154
A00286173 <A00286173@mycambrian.ca>
2025-06-15 14:53:33 -0400

    Added Authentication with ASP.NET Identity

--- ShoppingCart.API/Program.cs
+++ ShoppingCart.API/Program.cs
@@ -13,15 +13,22 @@ builder.Services.AddDbContext<AppSecurityDbContext>(options =>
     options.UseInMemoryDatabase("ShoppingCartAuthDb"));  // in-memory database for Identity
 
 
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+       .AddEntityFrameworkStores<AppSecurityDbContext>();
+
+//  Add authorization services
+builder.Services.AddAuthorization();
+
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -32,7 +39,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();   // enable authentication middleware
+
 app.UseAuthorization();
+
+// Map the built-in Identity minimal API endpoints
+app.MapIdentityApi<IdentityUser>();
 
 app.MapControllers();
 
